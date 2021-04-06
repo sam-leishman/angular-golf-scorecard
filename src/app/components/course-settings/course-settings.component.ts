3ec99b694bfda44dd48ba6b36d66ded11f34b250
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2021-04-06 09:27:15 -0600

    Added checks to make sure a user is entered before starting and there can't be multiple users with the same name

--- src/app/components/course-settings/course-settings.component.ts
+++ src/app/components/course-settings/course-settings.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CourseDataService } from 'src/app/services/course-data.service';
 import { PlayerSettingsService } from 'src/app/services/player-settings.service';
 
@@ -17,7 +17,10 @@ export class CourseSettingsComponent implements OnInit {
 
   availableHoles: any = [9, 18]
 
+  errorMessage: string;
+
   constructor(
+    private route: Router,
     private actRoute: ActivatedRoute,
     private courseDataService: CourseDataService,
     public fb: FormBuilder,
@@ -46,6 +49,16 @@ export class CourseSettingsComponent implements OnInit {
 
   submitForm() {
     this.playerSettingsService.playerSettings = this.form.value;
+
+    console.log(this.form.value)
+
+    if (this.form.value.player1 == '' && this.form.value.player2 == '' && this.form.value.player3 == '' && this.form.value.player4 == '') {
+      this.errorMessage = "There needs to be at least one player to play (duh)"
+    } else if (this.form.value.player1 == (this.form.value.player2 || this.form.value.player3 || this.form.value.player4) || this.form.value.player2 == (this.form.value.player1 || this.form.value.player3 || this.form.value.player4) || this.form.value.player3 == (this.form.value.player1 || this.form.value.player2 || this.form.value.player4)) {
+      this.errorMessage = "Players can't have the same name"
+    } else {
+      this.route.navigate(['/scorecard/', this.courseId])
+    }
   }
 
 }
